e4ccd2748c787fe87d0400346fa61b408e762d4c
khaibtros <trankhai15052005@gmail.com>
2024-12-29 09:47:25 +0700

    Create registerUser controller and do Route to it

---
+++ server/controllers/auth/auth-controller.js
@@ -0,0 +1,30 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
+
+const registerUser = async (req, res) => {
+    const { userName, email, password } = req.body;
+  
+    try {
+      const hashPassword = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        userName,
+        email,
+        password: hashPassword,
+      });
+  
+      await newUser.save();
+      res.status(200).json({
+        success: true,
+        message: "Registration successful",
+      });
+    } catch (e) {
+      console.log(e);
+      res.status(500).json({
+        success: false,
+        message: "Some error occured",
+      });
+    }
+  };
+
+  module.exports = { registerUser };
