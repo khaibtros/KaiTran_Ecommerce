9ee210c29b3cc5a33f48f933a15ece4cfc50b5d7
khaibtros <trankhai15052005@gmail.com>
2025-01-02 01:58:59 +0700

    LogoutUser and AuthMiddleware

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -39,6 +39,41 @@ export const loginUser = createAsyncThunk(
   }
 );
 
+export const logoutUser = createAsyncThunk(
+  "/auth/logout",
+
+  async () => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/logout",
+      {},
+      {
+        withCredentials: true,
+      }
+    );
+
+    return response.data;
+  }
+);
+
+export const checkAuth = createAsyncThunk(
+  "/auth/checkauth",
+
+  async () => {
+    const response = await axios.get(
+      "http://localhost:5000/api/auth/check-auth",
+      {
+        withCredentials: true,
+        headers: {
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
+        },
+      }
+    );
+
+    return response.data;
+  }
+);
+
 const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -73,6 +108,24 @@ const authSlice = createSlice({
           state.user = null;
           state.isAuthenticated = false;
         })
+        .addCase(checkAuth.pending, (state) => {
+          state.isLoading = true;
+        })
+        .addCase(checkAuth.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.user = action.payload.success ? action.payload.user : null;
+          state.isAuthenticated = action.payload.success;
+        })
+        .addCase(checkAuth.rejected, (state, action) => {
+          state.isLoading = false;
+          state.user = null;
+          state.isAuthenticated = false;
+        })
+        .addCase(logoutUser.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.user = null;
+          state.isAuthenticated = false;
+        });
     },
 })
 
