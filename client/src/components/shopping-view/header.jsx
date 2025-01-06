954f77a3b13fdd37925aa5ab830df1b341643908
khaibtros <trankhai15052005@gmail.com>
2025-01-06 22:52:55 +0700

    Start working on shopping-view, create basic ShoppingHeader

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,7 +1,30 @@
 function ShoppingHeader() {
+    const { isAuthenticated } = useSelector((state) => state.auth);
+  
     return (
-        <div>shopping header</div>
-    )
-}
+      <header className="sticky top-0 z-40 w-full border-b bg-background">
+        <div className="flex h-16 items-center justify-between px-4 md:px-6">
+          <Link to="/shop/home" className="flex items-center gap-2">
+            <HousePlug className="h-6 w-6" />
+            <span className="font-bold">Ecommerce</span>
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon" className="lg:hidden">
+                <Menu className="h-6 w-6" />
+                <span className="sr-only">Toggle header menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="w-full max-w-xs">
+                
+            </SheetContent>
+          </Sheet>
+          <div className="hidden lg:block">
 
-export default ShoppingHeader
+          </div>
+        </div>
+      </header>
+    );
+}
+  
+export default ShoppingHeader;  
