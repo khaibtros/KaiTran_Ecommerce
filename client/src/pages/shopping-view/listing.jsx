b0ac4ae5388bc1cee4b6ce7a2566866ae30a67ac
khaibtros <trankhai15052005@gmail.com>
2025-01-08 20:54:32 +0700

    add ProductFilter component to ShoppingListing

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,6 +1,10 @@
+import ProductFilter from "@/components/shopping-view/filter";
+
 function ShoppingListing() {
     return (
-      <div>Shopping listing</div>
+      <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
+        <ProductFilter/>
+      </div>
     );
   }
   
