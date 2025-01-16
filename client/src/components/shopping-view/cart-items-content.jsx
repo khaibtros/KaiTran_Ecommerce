ea4ff04db3c3dda9dd8dc0b451f076a25cfa620c
khaibtros <trankhai15052005@gmail.com>
2025-01-16 15:59:00 +0700

    Fix bugs: import useToast at cart-items-content.jsx + delete redundant review section in product-details (implement it fully later)

--- client/src/components/shopping-view/cart-items-content.jsx
+++ client/src/components/shopping-view/cart-items-content.jsx
@@ -2,7 +2,7 @@ import { Minus, Plus, Trash } from "lucide-react";
 import { Button } from "../ui/button";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteCartItem, updateCartQuantity } from "@/store/shop/cart-slice";
-import { useToast } from "../ui/use-toast";
+import { useToast } from "@/hooks/use-toast";
 
 function UserCartItemsContent({ cartItem }) {
   const { user } = useSelector((state) => state.auth);
