9fc292bc63ae4bd8c4bef946188f2988dc821bbc
khaibtros <trankhai15052005@gmail.com>
2025-01-18 14:31:21 +0700

    handleEditAddress, Toast alert, limit add 3 addresses only, and css when selecting a address

--- client/src/components/shopping-view/address-card.jsx
+++ client/src/components/shopping-view/address-card.jsx
@@ -1,14 +1,27 @@
 import { Button } from "../ui/button";
-import { Card, CardContent } from "../ui/card";
+import { Card, CardContent, CardFooter } from "../ui/card";
 import { Label } from "../ui/label";
 
 function AddressCard({
   addressInfo,
+  handleDeleteAddress,
   handleEditAddress,
-  handleDeleteAddress
+  setCurrentSelectedAddress,
+  selectedId,
 }) {
   return (
-    <Card>
+    <Card
+      onClick={
+        setCurrentSelectedAddress
+          ? () => setCurrentSelectedAddress(addressInfo)
+          : null
+      }
+      className={`cursor-pointer border-red-700 ${
+        selectedId?._id === addressInfo?._id
+          ? "border-red-900 border-[4px]"
+          : "border-black"
+      }`}
+    >
       <CardContent className="grid p-4 gap-4">
         <Label>Address: {addressInfo?.address}</Label>
         <Label>City: {addressInfo?.city}</Label>
