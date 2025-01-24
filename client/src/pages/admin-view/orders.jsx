9902d24c02a501764c2e0069b1ceaeb78b7e7498
khaibtros <trankhai15052005@gmail.com>
2025-01-24 15:10:52 +0700

    Admin Orders page

--- client/src/pages/admin-view/orders.jsx
+++ client/src/pages/admin-view/orders.jsx
@@ -1,5 +1,11 @@
+import AdminOrdersView from "@/components/admin-view/orders";
+
 function AdminOrders() {
-    return <div>admin orders</div>
+  return (
+    <div>
+      <AdminOrdersView />
+    </div>
+  );
 }
 
 export default AdminOrders;
