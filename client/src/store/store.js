59d004525d3d75a4d89ec553503723b312d0146b
khaibtros <trankhai15052005@gmail.com>
2025-01-24 16:35:15 +0700

    Order slice for admin

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import adminProductsSlice from "./admin/products-slice";
+import adminOrderSlice from "./admin/order-slice";
 
 import shopProductsSlice from "./shop/products-slice";
 import shopCartSlice from "./shop/cart-slice";
@@ -10,7 +11,10 @@ import shopOrderSlice from "./shop/order-slice";
 const store = configureStore({
   reducer: {
     auth: authReducer,
+
     adminProducts: adminProductsSlice,
+    adminOrder: adminOrderSlice,
+
     shopProducts: shopProductsSlice,
     shopCart: shopCartSlice,
     shopAddress: shopAddressSlice,
